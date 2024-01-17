b89cea8a3836a191e2968c7422102c37a0426aa1
Jason Wang <954014787@qq.com>
2024-01-17 23:15:28 +0800

    Debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,7 +18,7 @@ ssh-keyscan -H ${SSH_HOST} >> /root/.ssh/known_hosts
 # 以下命令假设您已经有了可用的SSH密钥和隧道配置
 # 请根据实际情况替换 SSH_USER, SSH_HOST, SSH_PORT, LOCAL_PORT, REMOTE_HOST 和 REMOTE_PORT
 
-exec autossh \
+exec autossh -4 \
     -M 0 \
     -o "ServerAliveInterval 30" \
     -o "ServerAliveCountMax 3" \
